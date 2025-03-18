d1a7934dce9bba33b4706316e4445c3c25d4892e
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-03-18 14:37:46 +0530

    split up multi-header tests; consistent tests writing; remove some dups

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,19 +6,17 @@ const OH = "https://sidvishnoi.com";
 const OI = "https://example.com";
 const OT = "https://www.example.org";
 
-type Origins = typeof OH | typeof OI | typeof OT;
-type TestOrigins = null | Origins;
+const feature = "fullscreen";
 
-type TestCaseSingleHeader = [
-	headerValue: string,
-	{ allowed: TestOrigins[]; notAllowed: TestOrigins[] },
-	allow?: string,
-];
-type TestCaseMultipleHeaders<T extends string = "fullscreen" | "bluetooth"> = [
-	headerValues: string,
-	Record<T, { allowed: TestOrigins[]; notAllowed: TestOrigins[] }>,
-	allow?: string,
-];
+type Origin = typeof OH | typeof OI | typeof OT;
+type TestOrigin = null | Origin;
+
+type TestCase = {
+	name?: string;
+	headerValue: string;
+	expectations: { allowed: TestOrigin[]; notAllowed: TestOrigin[] };
+	allow?: string;
+};
 
 describe("document.featurePolicy.allowsFeature", () => {
 	describe("no header", () => {
@@ -46,87 +44,61 @@ describe("document.featurePolicy.allowsFeature", () => {
 		});
 	});
 
-	describe("with header provided, defaultAllowlist self", () => {
-		const testCases: TestCaseSingleHeader[] = [
-			["*", { allowed: [null, OH, OI], notAllowed: [] }],
-			["self", { allowed: [null, OH], notAllowed: [OI] }],
-			["(self)", { allowed: [null, OH], notAllowed: [OI] }],
-			["()", { allowed: [], notAllowed: [null, OH, OI] }],
-			[`"${OH}"`, { allowed: [null, OH], notAllowed: [OI] }],
-			[`"${OI}"`, { allowed: [OI], notAllowed: [null, OH] }],
-			[`(self "${OI}")`, { allowed: [null, OH, OI], notAllowed: [] }],
+	describe("with header provided", () => {
+		const testCases: TestCase[] = [
+			{
+				headerValue: "*",
+				expectations: { allowed: [null, OH, OI], notAllowed: [] },
+			},
+			{
+				headerValue: "self",
+				expectations: { allowed: [null, OH], notAllowed: [OI] },
+			},
+			{
+				headerValue: "(self)",
+				expectations: { allowed: [null, OH], notAllowed: [OI] },
+			},
+			{
+				headerValue: "()",
+				expectations: { allowed: [], notAllowed: [null, OH, OI] },
+			},
+			{
+				headerValue: `"${OH}"`,
+				expectations: { allowed: [null, OH], notAllowed: [OI] },
+			},
+			{
+				headerValue: `"${OI}"`,
+				expectations: { allowed: [OI], notAllowed: [null, OH] },
+			},
+			{
+				headerValue: `("${OI}")`,
+				expectations: { allowed: [OI], notAllowed: [null, OH] },
+			},
+			{
+				headerValue: `(self "${OI}")`,
+				expectations: { allowed: [null, OH, OI], notAllowed: [] },
+			},
+			{
+				headerValue: `(self "${OH}")`,
+				expectations: { allowed: [null, OH], notAllowed: [OI] },
+			},
+			{
+				headerValue: `(self "${OT}")`,
+				expectations: { allowed: [null, OH], notAllowed: [OI] },
+			},
 		];
-		for (const [header, cases] of testCases) {
+		for (const { headerValue: header, expectations } of testCases) {
 			it(`Permissions-Policy: fullscreen=${header}`, () => {
 				const policy = new PermissionsPolicy({
 					origin: OH,
 					headerValue: `fullscreen=${header}`,
 					defaultAllowlist: { fullscreen: `'self'` },
 				});
-				for (const origin of cases.allowed) {
+				for (const origin of expectations.allowed) {
 					allowed(policy, "fullscreen", origin ?? undefined);
 				}
-				for (const origin of cases.notAllowed) {
+				for (const origin of expectations.notAllowed) {
 					notAllowed(policy, "fullscreen", origin ?? undefined);
-				}
-			});
-		}
-	});
-
-	describe("with multiple headers provided", () => {
-		const testCases: TestCaseMultipleHeaders<"fullscreen" | "bluetooth">[] = [
-			[
-				"fullscreen=*, bluetooth=*",
-				{
-					fullscreen: { allowed: [null, OH, OI], notAllowed: [] },
-					bluetooth: { allowed: [null, OH, OI], notAllowed: [] },
-				},
-			],
-			[
-				"fullscreen=*,bluetooth=()",
-				{
-					fullscreen: { allowed: [null, OH, OI], notAllowed: [] },
-					bluetooth: { allowed: [], notAllowed: [null, OH, OI] },
-				},
-			],
-			[
-				"fullscreen=(self),bluetooth=(self)",
-				{
-					fullscreen: { allowed: [null, OH], notAllowed: [OI] },
-					bluetooth: { allowed: [null, OH], notAllowed: [OI] },
-				},
-			],
-			[
-				`fullscreen=(self "${OH}"),bluetooth=(self "${OI}")`,
-				{
-					fullscreen: { allowed: [null, OH], notAllowed: [OI] },
-					bluetooth: { allowed: [null, OH, OI], notAllowed: [] },
-				},
-			],
-			[
-				`fullscreen=("${OI}"),bluetooth=(self "${OT}")`,
-				{
-					fullscreen: { allowed: [OI], notAllowed: [null, OH] },
-					bluetooth: { allowed: [null, OH], notAllowed: [OI] },
-				},
-			],
-		];
-
-		for (const [header, cases] of testCases) {
-			it(`Permissions-Policy: ${header}`, () => {
-				const policy = new PermissionsPolicy({
-					origin: OH,
-					headerValue: header,
-					defaultAllowlist: { fullscreen: `'self'` },
-				});
-
-				for (const [feature, expectations] of Object.entries(cases)) {
-					for (const origin of expectations.allowed) {
-						allowed(policy, feature, origin ?? undefined);
-					}
-					for (const origin of expectations.notAllowed) {
-						notAllowed(policy, feature, origin ?? undefined);
-					}
 				}
 			});
 		}
@@ -226,14 +198,30 @@ describe("same origin - iframe.featurePolicy.allowsFeature", () => {
 	describe("headers provided", () => {
 		describe('allow=""', () => {
 			const feature = "fullscreen";
-			const testCases: TestCaseSingleHeader[] = [
-				["*", { allowed: [null, OI], notAllowed: [OH, OT] }],
-				["self", { allowed: [null, OI], notAllowed: [OH, OT] }],
-				[`"${OI}"`, { allowed: [null, OI], notAllowed: [OH, OT] }],
-				["()", { allowed: [], notAllowed: [null, OI, OH, OT] }],
-				[`"${OH}"`, { allowed: [], notAllowed: [null, OI, OH, OT] }],
+			const testCases: TestCase[] = [
+				{
+					name: "no allow attribute, headers allow everything",
+					headerValue: "*",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					headerValue: "self",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					headerValue: `"${OI}"`,
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					headerValue: "()",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					headerValue: `"${OH}"`,
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
 			];
-			for (const [header, exp] of testCases) {
+			for (const { headerValue: header, expectations: exp } of testCases) {
 				it(`Permissions-Policy: ${feature}=${header}`, (t) => {
 					if (exp.allowed.length + exp.notAllowed.length === 0) {
 						t.skip("no expectations");
@@ -255,98 +243,92 @@ describe("same origin - iframe.featurePolicy.allowsFeature", () => {
 			}
 		});
 
-		describe("multiple headers & allow attribute provided", () => {
-			type TestCase = [
-				name: string,
-				headerValue: TestCaseMultipleHeaders["0"],
-				allowValue: string,
-				expectations: TestCaseMultipleHeaders["1"],
-			];
+		describe("allow attribute provided", () => {
 			const testCases: TestCase[] = [
-				[
-					"no allow attribute, headers allow everything",
-					"fullscreen=*,bluetooth=*",
-					"",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
-				[
-					"subset of header",
-					"fullscreen=*,bluetooth=()",
-					"fullscreen *",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header too strict - disallow everything",
-					"fullscreen=(),bluetooth=()",
-					"fullscreen *; bluetooth 'src'",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header only allow self, allow as default-empty",
-					"fullscreen=(self),bluetooth=(self)",
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
-				[
-					"header allow self + 1",
-					`fullscreen=(self "${OH}"),bluetooth=(self "${OI}")`,
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
-				[
-					"header allow other only",
-					`fullscreen=("${OI}"),bluetooth=("${OT}")`,
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header allow many, iframe restricts",
-					`fullscreen=*,bluetooth=(self "${OI}" "${OT}")`,
-					`fullscreen ${OT}; bluetooth ${OI}`,
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
+				{
+					name: "subset of header",
+					headerValue: "fullscreen=*",
+					allow: "fullscreen *",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header too strict but allow *",
+					headerValue: "fullscreen=()",
+					allow: "fullscreen *",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header too strict and allow src",
+					headerValue: "fullscreen=()",
+					allow: "fullscreen 'src'",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header only allow self, allow as default-empty",
+					headerValue: "fullscreen=(self)",
+					allow: "fullscreen;",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header allow self + 1 #1",
+					headerValue: `fullscreen=(self "${OH}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header allow self + 1 #2",
+					headerValue: `fullscreen=(self "${OI}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header allow other only #1",
+					headerValue: `fullscreen=("${OI}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header allow other only #2",
+					headerValue: `fullscreen=("${OT}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow many, iframe restricts #1",
+					headerValue: "fullscreen=*",
+					allow: `fullscreen ${OT};`,
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow many, iframe restricts #2",
+					headerValue: `fullscreen=(self "${OI}" "${OT}")`,
+					allow: `fullscreen ${OI}`,
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
 			];
 
-			for (const [name, header, allow, cases] of testCases) {
+			for (const {
+				name,
+				headerValue: header,
+				allow = "",
+				expectations: exp,
+			} of testCases) {
 				it(`${name} {Permissions-Policy: ${header} & allow=${allow}}`, (t) => {
+					if (exp.allowed.length + exp.notAllowed.length === 0) {
+						t.skip("no expectations");
+					}
+
 					const policy = new PermissionsPolicy({
 						origin,
 						headerValue: header,
 						defaultAllowlist: { fullscreen: `'self'` },
 					}).inherit({ allow, origin });
 
-					for (const [feature, exp] of Object.entries(cases)) {
-						if (exp.allowed.length + exp.notAllowed.length === 0) {
-							t.skip("no expectations");
-						}
-
-						for (const origin of exp.allowed) {
-							allowed(policy, feature, origin ?? undefined);
-						}
-						for (const origin of exp.notAllowed) {
-							notAllowed(policy, feature, origin ?? undefined);
-						}
+					for (const origin of exp.allowed) {
+						allowed(policy, feature, origin ?? undefined);
+					}
+					for (const origin of exp.notAllowed) {
+						notAllowed(policy, feature, origin ?? undefined);
 					}
 				});
 			}
@@ -447,14 +429,30 @@ describe("cross origin - iframe.featurePolicy.allowsFeature", () => {
 
 	describe("headers provided", () => {
 		describe('allow=""', () => {
-			const testCases: TestCaseSingleHeader[] = [
-				["*", { allowed: [], notAllowed: [null, OI, OH, OT] }],
-				["self", { allowed: [], notAllowed: [null, OI, OH, OT] }],
-				[`"${OH}"`, { allowed: [], notAllowed: [null, OI, OH, OT] }],
-				["()", { allowed: [], notAllowed: [null, OI, OH, OT] }],
-				[`"${OI}"`, { allowed: [], notAllowed: [null, OI, OH, OT] }],
+			const testCases: TestCase[] = [
+				{
+					name: "no allow attribute, headers allow everything",
+					headerValue: "*",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					headerValue: "self",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					headerValue: `"${OH}"`,
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					headerValue: "()",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					headerValue: `"${OI}"`,
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
 			];
-			for (const [header, expectations] of testCases) {
+			for (const { headerValue: header, expectations } of testCases) {
 				it(`Permissions-Policy: fullscreen=${header}`, () => {
 					const policy = new PermissionsPolicy({
 						origin: OH,
@@ -472,97 +470,97 @@ describe("cross origin - iframe.featurePolicy.allowsFeature", () => {
 			}
 		});
 
-		describe("multiple headers & allow attribute provided", () => {
-			type TestCase = [
-				name: string,
-				headerValue: TestCaseMultipleHeaders["0"],
-				allowValue: string,
-				expectations: TestCaseMultipleHeaders["1"],
-			];
+		describe("allow attribute provided", () => {
 			const testCases: TestCase[] = [
-				[
-					"no allow attribute, headers allow everything",
-					"fullscreen=*,bluetooth=*",
-					"",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"subset of header",
-					"fullscreen=*,bluetooth=()",
-					"fullscreen *",
-					{
-						fullscreen: { allowed: [null, OI], notAllowed: [OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header too strict - disallow everything",
-					"fullscreen=(),bluetooth=()",
-					"fullscreen *; bluetooth 'src'",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header only allow self, allow=src",
-					"fullscreen=(self),bluetooth=(self)",
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header allow self + 1",
-					`fullscreen=(self "${OH}"),bluetooth=(self "${OI}")`,
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
-				[
-					"header allow other only",
-					`fullscreen=("${OH}"),bluetooth=("${OT}")`,
-					"fullscreen; bluetooth",
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [], notAllowed: [null, OI, OH, OT] },
-					},
-				],
-				[
-					"header allow many, iframe restricts",
-					`fullscreen=*,bluetooth=(self "${OI}" "${OT}")`,
-					`fullscreen ${OT}; bluetooth ${OI}`,
-					{
-						fullscreen: { allowed: [], notAllowed: [null, OI, OH, OT] },
-						bluetooth: { allowed: [null, OI], notAllowed: [OH, OT] },
-					},
-				],
+				{
+					name: "subset of header",
+					headerValue: "fullscreen=*",
+					allow: "fullscreen *",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "subset of header",
+					headerValue: "fullscreen=()",
+					allow: "",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header too strict - disallow everything #1",
+					headerValue: "fullscreen=()",
+					allow: "fullscreen *;",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header too strict - disallow everything  #2",
+					headerValue: "fullscreen=()",
+					allow: "fullscreen 'src'",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header only allow self, allow=src",
+					headerValue: "fullscreen=(self)",
+					allow: "fullscreen;",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow self + 1 #1",
+					headerValue: `fullscreen=(self "${OH}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow self + 1 #2",
+					headerValue: `fullscreen=(self "${OI}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
+				{
+					name: "header allow other only #1",
+					headerValue: `fullscreen=("${OH}")`,
+					allow: "fullscreen;",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow other only #2",
+					headerValue: `fullscreen=("${OT}")`,
+					allow: "fullscreen",
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow many, iframe restricts #1",
+					headerValue: "fullscreen=*",
+					allow: `fullscreen ${OT};`,
+					expectations: { allowed: [], notAllowed: [null, OI, OH, OT] },
+				},
+				{
+					name: "header allow many, iframe restricts #2",
+					headerValue: `fullscreen=(self "${OI}" "${OT}")`,
+					allow: `fullscreen ${OI}`,
+					expectations: { allowed: [null, OI], notAllowed: [OH, OT] },
+				},
 			];
 
-			for (const [name, header, allow, cases] of testCases) {
-				it(`${name} {Permissions-Policy: ${header} & allow=${allow}}`, (t) => {
+			for (const {
+				name,
+				headerValue,
+				allow = "",
+				expectations: exp,
+			} of testCases) {
+				it(`${name} {Permissions-Policy: ${headerValue} & allow=${allow}}`, (t) => {
 					const policy = new PermissionsPolicy({
 						origin: OH,
-						headerValue: header,
+						headerValue,
 						defaultAllowlist: { fullscreen: `'self'` },
 					}).inherit({ allow, origin: OI });
 
-					for (const [feature, exp] of Object.entries(cases)) {
-						if (exp.allowed.length + exp.notAllowed.length === 0) {
-							t.skip("no expectations");
-						}
-						for (const origin of exp.allowed) {
-							allowed(policy, feature, origin ?? undefined);
-						}
-						for (const origin of exp.notAllowed) {
-							notAllowed(policy, feature, origin ?? undefined);
-						}
+					if (exp.allowed.length + exp.notAllowed.length === 0) {
+						t.skip("no expectations");
+					}
+					for (const origin of exp.allowed) {
+						allowed(policy, feature, origin ?? undefined);
+					}
+					for (const origin of exp.notAllowed) {
+						notAllowed(policy, feature, origin ?? undefined);
 					}
 				});
 			}
